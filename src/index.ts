71658ded7fae0a9dec8b5840b0af137d39da55da
Bryan Cole <brycole@gmail.com>
2023-01-09 18:38:18 +0000

    check sol balance and airdrop

--- src/index.ts
+++ src/index.ts
@@ -29,6 +29,35 @@ async function initializeKeypair(connection: Web3.Connection): Promise<Web3.Keyp
   }
 
 
+async function airdropSolIfNeeded(
+    signer: Web3.Keypair,
+    connection: Web3.Connection
+  ) {
+    const balance = await connection.getBalance(signer.publicKey);
+    console.log('Current balance is', balance / Web3.LAMPORTS_PER_SOL, 'SOL');
+  
+    // 1 SOL should be enough for almost anything you wanna do
+    if (balance / Web3.LAMPORTS_PER_SOL < 1) {
+      // You can only get up to 2 SOL per request 
+      console.log('Airdropping 1 SOL');
+      const airdropSignature = await connection.requestAirdrop(
+        signer.publicKey,
+        Web3.LAMPORTS_PER_SOL
+      );
+  
+      const latestBlockhash = await connection.getLatestBlockhash();
+  
+      await connection.confirmTransaction({
+        blockhash: latestBlockhash.blockhash,
+        lastValidBlockHeight: latestBlockhash.lastValidBlockHeight,
+        signature: airdropSignature,
+      });
+  
+      const newBalance = await connection.getBalance(signer.publicKey);
+      console.log('New balance is', newBalance / Web3.LAMPORTS_PER_SOL, 'SOL');
+    }
+}
+
 main()
     .then(() => {
         console.log("Finished successfully")
