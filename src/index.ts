4bffbfcbfab0de2ff1d9fb6519fea644ec695029
Bryan Cole <brycole@gmail.com>
2023-01-10 18:03:39 +0000

    added ping transaction

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,17 @@ import * as fs from 'fs';
 import dotenv from 'dotenv';
 dotenv.config();
 
+const PROGRAM_ID = new Web3.PublicKey("ChT1B39WKLS8qUrkLvFDXMhEJ4F1XZzwUNHUt4AU9aVa")
+const PROGRAM_DATA_PUBLIC_KEY = new Web3.PublicKey("Ah9K7dQ8EHaZqcAsgBW8w37yN2eAy3koFmUn4x3CJtod")
+
 async function main() {
     const connection = new Web3.Connection(Web3.clusterApiUrl('devnet'));
     const signer = await initializeKeypair(connection);
   
     console.log("Public key:", signer.publicKey.toBase58());
     await airdropSolIfNeeded(signer, connection);
+
+    await pingProgram(connection, signer);
 }
 
 
@@ -58,6 +63,34 @@ async function airdropSolIfNeeded(
       const newBalance = await connection.getBalance(signer.publicKey);
       console.log('New balance is', newBalance / Web3.LAMPORTS_PER_SOL, 'SOL');
     }
+}
+
+async function pingProgram(connection: Web3.Connection, payer: Web3.Keypair) {
+  const transaction = new Web3.Transaction()
+  const instruction = new Web3.TransactionInstruction({
+    // Instructions need 3 things 
+    
+    // 1. The public keys of all the accounts the instruction will read/write
+    keys: [
+      {
+        pubkey: PROGRAM_DATA_PUBLIC_KEY,
+        isSigner: false,
+        isWritable: true
+      }
+    ],
+    
+    // 2. The ID of the program this instruction will be sent to
+    programId: PROGRAM_ID
+    
+    // 3. Data - in this case, there's none!
+  })
+
+  transaction.add(instruction)
+  const transactionSignature = await Web3.sendAndConfirmTransaction(connection, transaction, [payer])
+
+  console.log(
+    `Transaction https://explorer.solana.com/tx/${transactionSignature}?cluster=devnet`
+    )
 }
 
 main()
